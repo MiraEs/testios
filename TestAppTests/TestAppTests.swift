6e4cc9c3c11cc12dedb80a7f1de8cfcd5d04d997
MiraEs <imz.estil@gmail.com>
2021-04-16 15:02:31 -0400

    xcasserttrue & xcassertfalse

--- TestAppTests/TestAppTests.swift
+++ TestAppTests/TestAppTests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class TestAppTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_fail_withSimpleMessage() {
+        XCTFail("We have a problem")
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_fail_withInterpolatedMessage() {
+        let theAnswer = 42
+        XCTFail("The answer to the question is \(theAnswer)")
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_assertTrue() {
+        let success = true
+        XCTAssertTrue(success)
     }
 
 }
